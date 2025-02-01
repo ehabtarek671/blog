64961ceef34858be4b6b86100ea2f108202111f1
ehabtarek671 <93053798+ehabtarek671@users.noreply.github.com>
2025-02-01 17:03:02 +0200

    Enhance

--- blog/func/saveimage.py
+++ blog/func/saveimage.py
@@ -1,4 +1,4 @@
 
 def user_profile_image_path(instance, filename):
     """Stores profile images in a folder named after the user's email."""
-    return f'media/{instance.email}/{filename}'
+    return f'{instance.email}/{filename}'
