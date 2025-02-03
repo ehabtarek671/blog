48d5b100cdb13f9abd982665b5305bbf7472294a
Mostafa Ahmed <mostafa.ahmed.m.2009@gmail.com>
2025-02-03 21:34:31 +0200

    Log In Page with JS

--- blog/home/static/JS/login.js
+++ blog/home/static/JS/login.js
@@ -1,23 +1,45 @@
 const API = '/login/';
 const form = document.getElementById('login-form');
 
+function getCSRFToken() {
+    const csrfToken = document.cookie.split('; ').find((item) => item.startsWith('csrftoken=')).split('=')[1];
+    return csrfToken;
+}
+
 form.addEventListener('submit', (e) => {
+    e.preventDefault();
     const formData = new FormData(form);
+    const csrfToken = getCSRFToken();
 
     fetch(API, {
         method: 'POST',
         headers: {
-            'Content-Type': 'multipart/form-data',
+            'Content-Type': 'application/json',
+            'X-CSRFToken': `${csrfToken}`
         },
-        body: formData,
+        body: JSON.stringify({
+            email: `${formData.get('user_email')}`,
+            pwd: `${formData.get('user_password')}`,
+        })
     })
     .then(response => response.json())
     .then((data) => {
         if (data.message === 'Error') {
-            e.preventDefault();
-            console.log('Error, Credentials you entered are wrong');
+
+            const inputs = document.querySelectorAll('.input-field input');
+            const errorBox = document.querySelector('.error-box');
+            errorBox.setAttribute('id', 'visible');
+            setTimeout(() => {
+                errorBox.removeAttribute('id');
+            }, 5000);
+            inputs.forEach((input) => {
+                input.classList.add('error');
+            });
+
         } else if (data.message === 'Accepted') {
+
             window.location.replace('/');
+
         }
     })
     .catch(err => console.error(err));
--- blog/home/static/JS/signup.js
+++ blog/home/static/JS/signup.js
@@ -1,18 +1,32 @@
 const userImage = document.getElementById('user-image');
 const canvas = document.getElementById('canvas');
 const context = canvas.getContext('2d');
-
+ 
 userImage.addEventListener('change', () => {
     const image = userImage.files[0];
-    const newImage = new Image(500, 500);
+    const newImage = new Image();
+
     newImage.addEventListener('load', () => {
-        canvas.width = newImage.width;
-        canvas.height = newImage.height;
+        const maxWidth = 500;
+        const maxHeight = 500;
 
-        context.drawImage(newImage, 0, 0);
-
+        let width = newImage.width;
+        let height = newImage.height;
+ 
+        if (width > maxWidth || height > maxHeight) {
+            const scale = Math.min(maxWidth / width, maxHeight / height);
+            width *= scale;
+            height *= scale;
+        }
+ 
+        canvas.width = width;
+        canvas.height = height;
+ 
+        context.drawImage(newImage, 0, 0, width, height);
+ 
         URL.revokeObjectURL(newImage.src);
     });
+ 
     newImage.src = URL.createObjectURL(image);
     canvas.style.display = 'block';
 });
